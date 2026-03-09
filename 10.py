1309e7aca4fdac76e7b79894ae2839879e70e107
canorHVH <egorka.tok47@gmail.com>
2026-03-09 18:15:55 +0200

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,6 @@
+a = input("enter password:")
+
+if a == "password123":
+    print("welcom to system")
+else:
+    print("failed")
---
+++ 3.py
@@ -0,0 +1,6 @@
+name = input()
+age = int(input())
+
+years = 100 - age
+
+print(name, "буде 100 років через", years, "років")
